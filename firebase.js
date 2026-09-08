83ae9a0d8c61453d73e09c6d4993d86bc7812513
lokojagrocerymall <lokojagrocerymall@gmail.com>
2026-09-08 11:38:41 +0100

    Fix firebase config

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
-﻿firebase.js
-// Lokoja Grocery Mall - Firebase Connection
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyChrVZh6NUm7N5W7FMXnxztmU7NXRYrVa0",
-  authDomain: "lokoja-grocery-mall.firebaseapp.com",
-  projectId: "lokoja-grocery-mall",
-  storageBucket: "lokoja-grocery-mall.firebasestorage.app",
-  messagingSenderId: "642467126038",
-  appId: "1:642467126038:web:420d7c0e33119b7fa24e37"
+  apiKey: "AIzaSyChrVZh6NUm7N5W7FMXnxztmU7NXRYr...", // paste your FULL new key here
+  authDomain: "lokoja-grocery-mall.firebaseapp.com",
+  projectId: "lokoja-grocery-mall",
+  storageBucket: "lokoja-grocery-mall.firebasestorage.app",
+  messagingSenderId: "642467126038",
+  appId: "1:642467126038:web:420d7c0e33119b7fa24e37"
 };
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
